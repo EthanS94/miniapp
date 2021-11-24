2532e021716977e0c72c811271873d44d4feb21f
David Rogers <dhr@lanl.gov>
2021-11-24 12:06:54 -0500

    updates for spack build

--- run/submit.sh
+++ run/submit.sh
@@ -5,7 +5,9 @@
 #BSUB -P <compute_allocation> 
 #BSUB -W 00:10
 
-# module load gcc/10.2.0
+module purge
 module load gcc/9.3.0
+module load openmpi-4.1.1-gcc-9.3.0-4rgdyx2
+module list
 
 jsrun -n2 <pantheon_run_dir>/cloverleaf3d_par
